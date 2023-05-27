c2d72353fc722995b4f7e67c18a4f6937ad34081
Eguchi <126472433+eguchoii@users.noreply.github.com>
2023-05-26 21:28:38 -0300

    Update Algoritmo 2.go

--- Algoritmo 2.go
+++ Algoritmo 2.go
@@ -1,5 +1,3 @@
-//Crie uma struct chamada Pessoa com os campos "nome", "idade" e "endereço". O campo "endereço" deve ser uma outra struct com os campos "rua", "número", "cidade" e "estado". Escreva uma função que receba uma Pessoa como parâmetro e imprima seu endereço completo.
-
 package main
 
 import "fmt"
